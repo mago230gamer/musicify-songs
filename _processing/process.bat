727ae7e58d6da15c1e3b665e5e9a0237a8e65e49
EmmaKnijn <rubenknijn@protonmail.com>
2022-10-07 16:57:34 +0200

    fix sound

--- _processing/process.bat
+++ _processing/process.bat
@@ -1,4 +1,3 @@
-ffmpeg -i %1 %1.wav
-java -jar Lionray.jar %1.wav %1.dfpwm
+ffmpeg -i %1 -y -acodec dfpwm -ac 1 -ar 48000 -vn -fs 25000000 -f dfpwm %1.dfpwm
 ::ffmpeg -i %1 -af "pan=mono|c0=FR" %1-right.dfpwm
 ::ffmpeg -i %1 -af "pan=mono|c0=FL" %1-left.dfpwm
