aa3ffd458597c43b5ab1943690d9414dedc18852
EmmaKnijn <rubenknijn@protonmail.com>
2022-10-07 16:40:51 +0200

    stufff:tm:

--- _processing/process.bat
+++ _processing/process.bat
@@ -1,3 +1,4 @@
-ffmpeg -i %1 %1.dfpwm
+ffmpeg -i %1 %1.wav
+java -jar Lionray.jar %1.wav %1.dfpwm
 ::ffmpeg -i %1 -af "pan=mono|c0=FR" %1-right.dfpwm
 ::ffmpeg -i %1 -af "pan=mono|c0=FL" %1-left.dfpwm
